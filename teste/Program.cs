3193776cec3e09585acf5dd458fd386f56920c5a
Rogerio <rogerio.davcardoso@gmail.com>
2019-01-05 15:22:55 -0200

    Acrescentada uma mensagem de bom dia!

--- teste/Program.cs
+++ teste/Program.cs
@@ -5,6 +5,8 @@ namespace teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
 
+            Console.WriteLine("Bom dia!");
+
             Console.ReadLine();
         }
     }
